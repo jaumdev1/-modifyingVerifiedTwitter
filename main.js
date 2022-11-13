c1b4dea97e3d623923697f91f0c026d120cbf349
João Antônio <56412536+joao-dev1@users.noreply.github.com>
2022-11-13 11:30:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ else twitterBlueVerified();
 function twitterBlueVerified(){
     setInterval(function () {document.querySelectorAll("g").forEach(element => {
         var el = element.innerHTML.substring(9,15);
-        if(el == "M22.25"){
+        if(el == "M22.25")
             element.parentElement.parentElement.innerHTML = "<p>💀</p>"
 
-        }
+        
     });}, 1000);
 }
