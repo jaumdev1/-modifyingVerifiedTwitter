03d209ff6d5a9882d1709d49ff17a98487944736
João Antônio <56412536+joao-dev1@users.noreply.github.com>
2022-11-13 11:11:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+if(document.readyState == "loading") document.addEventListener('DOMContentLoaded', twitterBlueVerified);
+else twitterBlueVerified();
+
+function twitterBlueVerified(){
+    setInterval(function () {document.querySelectorAll("g").forEach(element => {
+        var el = element.innerHTML.substring(9,15);
+        if(el == "M22.25"){
+            element.parentElement.parentElement.innerHTML = "<p>💀</p>"
+
+        }
+    });}, 1000);
+}
